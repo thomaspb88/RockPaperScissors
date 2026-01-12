5edbcfb834323b6e82ba32f447120ed1e5e0eae1
Tom Barratt <Tom@mediasterling.com>
2026-01-12 16:09:58 +0000

    Adding ui and updated js to accomodate for user interaction

--- js/javascript.js
+++ js/javascript.js
@@ -4,11 +4,17 @@ const resultMatrix = [
     ["lose", "win", "draw"]
 ];
 
+const scores = {
+    win: 0,
+    lose: 0,
+    draw: 0
+};
+
 function ResultCalculator(userIndex, computerIndex) {
     return resultMatrix[userIndex][computerIndex];
 }
 
-function userAnswerTranslator(userInput) {
+function mapUserChoiceToIndex(userInput) {
     if (userInput === "rock") return 0;
     if (userInput === "paper") return 1;
     if (userInput === "scissors") return 2;
@@ -19,18 +25,49 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function getHumanChoice() {
-    return prompt("Enter rock, paper, or scissors:");
+function play(userInput) {
+    const computerNumberChoice = getComputerChoice();
+
+    let userChoice = userInput.toLowerCase();
+    let computerChoice = ["rock", "paper", "scissors"][computerNumberChoice];
+
+    console.log("User chose: " + userChoice);
+    console.log("Computer chose: " + computerChoice);
+    const result = ResultCalculator(mapUserChoiceToIndex(userChoice), computerNumberChoice);
+    console.log("Result: " + result);
+    
+    displayResult(result);
+    updateScores(result);
+    updateDisplay(userChoice, computerChoice);
 }
 
-function playGame() {
-    let userInput = getHumanChoice();
-    const userAnswer = userAnswerTranslator(userInput);
-    const computerAnswer = getComputerChoice();
-    console.log("User chose: " + ["rock", "paper", "scissors"][userAnswer]);
-    console.log("Computer chose: " + ["rock", "paper", "scissors"][computerAnswer]);
-    const result = ResultCalculator(userAnswer, computerAnswer);
-    console.log("Result: " + result);
+function updateScores(result){
+    const playerScoreElem = document.getElementById('player-score-value');
+    const computerScoreElem = document.getElementById('computer-score-value');
+    const drawScoreElem = document.getElementById('draw-score-value');
+
+    let playerScore = parseInt(playerScoreElem.textContent);
+    let computerScore = parseInt(computerScoreElem.textContent);
+    let drawScore = parseInt(drawScoreElem.textContent);
+    if(result === 'win'){
+        playerScore += 1;
+    } else if(result === 'lose'){
+        computerScore += 1;
+    } else if(result === 'draw'){
+        drawScore += 1;
+    }
+    playerScoreElem.textContent = playerScore;
+    computerScoreElem.textContent = computerScore;
+    drawScoreElem.textContent = drawScore;
+}
+
+function updateDisplay(userChoice, computerChoice){
+    document.getElementById('player-choice').textContent = `${userChoice}`;
+    document.getElementById('computer-choice').textContent = `${computerChoice}`;
+}
+
+function displayResult(result){
+    document.getElementById('result-message').textContent = `You ${result}!`;
 }
 
 function camelise(str){
